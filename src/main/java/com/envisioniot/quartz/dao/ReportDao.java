8584665c7cc370cb71315875caf90afb975334bf
haojie.yang2 <haojie.yang2@envision-digital.com>
2020-09-01 21:15:14 +0800

    quartz - 基本任务

---
+++ src/main/java/com/envisioniot/quartz/dao/ReportDao.java
@@ -0,0 +1,12 @@
+package com.envisioniot.quartz.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.envisioniot.quartz.entity.Report;
+
+/**
+ * @author YangHaojie
+ * @date 2020/9/1
+ */
+public interface ReportDao extends BaseMapper<Report> {
+
+}
